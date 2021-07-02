8ec30ccf4a14f006472cfb180f35297375c047b9
juanscarlata2 <56403767+juanscarlata2@users.noreply.github.com>
2021-07-01 20:05:10 -0500

    Update sources Semana7
    Se agrega la clase para trabajar los datos de productos como un archivo CSV

---
+++ Semana7/Sources/data_csv.py
@@ -0,0 +1,47 @@
+import csv
+
+class dataClients(object):
+    def __init__(self, file_name):
+        self.name_file=file_name
+        self.tabla=[]
+        self.fieldnames=['id', 'nombre', 'cantidad', 'precio']
+
+
+    def read(self):
+        with open(self.name_file) as csv_file:
+            datos=csv.DictReader(csv_file, fieldnames=self.fieldnames)
+            for row in datos:
+                self.tabla.append(row)
+            
+
+    def write_rwo(self, datos):
+
+        with open(self.name_file, 'a') as csv_file:
+            writer=csv.DictWriter(csv_file, fieldnames=self.fieldnames)
+            if isinstance(datos, dict):
+                try:
+                    writer.writeheader()
+                    writer.writerow(datos)
+                except:
+                    print("No tiene las keys correctas")
+                    return False
+                else:
+                    return True
+            else:
+                print("No es un dict")
+                return False 
+
+
+mi_tabla=dataClients(file_name='Datos.csv') #Se crea un objeto de la clase
+
+dato={'id':'0001', 'nombre':'platano', 'cantidad':34, 'precio':2000}
+
+validar=mi_tabla.write_rwo(dato)
+
+print(validar)
+
+
+
+
+    
+            
